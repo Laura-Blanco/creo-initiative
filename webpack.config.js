2d1a66134ec944a2d8bbebf8ae5479cf88423575
Laura-Blanco <60363157+Laura-Blanco@users.noreply.github.com>
2024-03-27 23:46:11 -0600

    add webpack in order to deploy to netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); 
 const path = require('path');
 
 module.exports = {
-  entry: './src/pages/Home.js',
+  entry: [
+    './src/Home.js',
+    './src/style.css'
+  ],
   output: {
+    path: path.resolve(__dirname, "dist"),
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
   },
-};
+  devServer: {
+    static: [
+      {
+        directory: path.resolve(__dirname),
+        watch: true,
+      },
+      {
+        directory: path.resolve(__dirname, 'src'),
+        watch: true,
+      }
+    ],
+    port: 8000,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'config.json', to: 'config.json' },
+        { from: 'src/images', to: 'images' },
+      ]
+    })
+  ]
+}
